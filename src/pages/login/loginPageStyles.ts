9542f970d551d1c9454a13953aa243a28cc5f4c1
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-16 00:46:25 +0800

    added styles for login page

---
+++ src/pages/login/loginPageStyles.ts
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const Styles = makeStyles(() => ({
+    loginContainer: {
+        padding: 50,
+    },
+    loginForm: {
+        justifyContent: 'normal',
+        minHeight: '90vh'
+    },
+    buttonBlock: {
+        width: '100%',
+        padding: 10,
+    },
+    loginBackground: {
+        justifyContent: 'center',
+        minHeight: '30vh',
+        padding: 50,
+    },
+}))
+
+export default Styles
