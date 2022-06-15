ebb9f4285dd779d46f140aca9866e875968ee497
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-16 00:44:12 +0800

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Index from "./pages/book/indexPage";
+import User from "./pages/user/user";
+import LoginPage from "./pages/login/loginPage";
+import Dashboard from "./pages/Dashboard/dashboardPage";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path='/index' element={<Index />} />
+        <Route path='/user' element={<User />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
