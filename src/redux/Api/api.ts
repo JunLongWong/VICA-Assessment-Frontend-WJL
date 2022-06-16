3c2d8f41cd9e7abeae8ba5f0092e2316a01ad430
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-16 21:55:07 +0800

    added api's

---
+++ src/redux/Api/api.ts
@@ -0,0 +1,64 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { RootState } from '../store';
+import { IBook, IUser } from './types';
+
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3000',
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+
+      return headers
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getUser: builder.query<{ user: IUser }, string>({
+      query: (id) => ({ url: `users/${id}` }),
+
+    }),
+    getAllUser: builder.query<{ users: { user: IUser[] } }, null>({
+      query: () => ({ url: `users/` }),
+    }),
+    deleteUser: builder.mutation<{ user: IUser }, string>({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: "DELETE"
+      })
+    }),
+
+    createUser: builder.mutation<{ user: IUser }, null>({
+      query: (user) => ({
+        url: `users`,
+        method: "POST",
+        body: user
+      })
+    }),
+    updateUser: builder.mutation<{ user: IUser }, IUser>({
+      query: ({ _id, ...rest }) => ({
+        url: `users/${_id}`,
+        method: "PUT",
+        body: rest
+      })
+    }),
+
+    getAllBook: builder.query<{ Books: { bookList: IBook[] } }, null>({
+      query: () => ({ url: `books/` }),
+    }),
+
+  }),
+})
+
+export const {
+  useGetUserQuery,
+  useGetAllUserQuery,
+  useDeleteUserMutation,
+  useCreateUserMutation,
+  useUpdateUserMutation,
+  useGetAllBookQuery,
+} = api
