8367832cee5afd4e72606809989e53b21b7c7491
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-20 19:57:19 +0800

    added imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Index from "./pages/book/indexPage";
-import User from "./pages/user/user";
-import LoginPage from "./pages/login/loginPage";
-import Dashboard from "./pages/Dashboard/dashboardPage";
+import Index from "./pages/book/IndexPage";
+import User from "./pages/user/UserPage";
+import LoginPage from "./pages/login/LoginPage";
+import Dashboard from "./pages/Dashboard/DashboardPage";
 import { Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
