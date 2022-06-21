bd7b280047a2bb9ff04187feec9e37ebfcfaf544
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-21 14:22:03 +0800

    deleted unused file

--- src/utils/DashboardDataTransform.ts
+++
@@ -1,9 +0,0 @@
-export const dataTransform = (dataArrlist: any[], type: string) => {
-    const dataArr: any[] = []
-
-    dataArrlist.map(obj => {
-        dataArr.push({ name: obj.type, Quantity: obj.quantity })
-    })
-
-    return dataArr
-}
