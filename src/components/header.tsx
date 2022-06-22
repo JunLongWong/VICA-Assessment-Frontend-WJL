27bcc51e1db480e5a2d9bf24a2d0d5191ef32423
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-22 17:53:05 +0800

    added header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,67 @@
 import {
-    Grid,
-    AppBar,
-    Typography,
-    Toolbar,
+    Grid
 } from '@material-ui/core';
+import { Button, Stack, Typography } from '@mui/material';
+import { useAuth } from '../hooks/useAuth';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
     headerName: string
 }
 
 const AppBarHeader: React.FC<Props> = ({ headerName }: Props) => {
+    const {
+        loggedUser,
+        logout,
+        getLoggedInUserRole,
+    } = useAuth();
+
+    const navigate = useNavigate()
+
+    const handleDashboard = () => {
+        navigate('/dashboard')
+    }
+    const handleUsers = () => {
+        navigate('/user')
+    }
+    const handleBooks = () => {
+        navigate('/index')
+    }
+
     return (
         <div>
             <AppBar position="static" color="primary">
                 <Toolbar>
+                    <Grid>
+                        <Button color="inherit" onClick={handleUsers}>
+                            User
+                        </Button>
+                    </Grid>
+
+                    <Grid>
+                        <Button color="inherit" onClick={handleBooks}>
+                            Book
+                        </Button>
+                    </Grid>
+                    <Grid>
+                        <Button color="inherit" onClick={handleDashboard}>
+                            Dashboard
+                        </Button>
+                    </Grid>
                     <Grid container justifyContent="center" wrap="wrap">
                         <Grid item>
                             <Typography variant="h6">{headerName}</Typography>
                         </Grid>
                     </Grid>
+
+                    <Stack direction="row" spacing={2}>
+                        <Button variant="contained" color="error" onClick={logout}>
+                            Logout
+                        </Button>
+                    </Stack>
                 </Toolbar>
             </AppBar>
         </div>
