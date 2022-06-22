28c11b73e5ac845bb78f77caed36257fb792b431
Jun Long <junlong@Juns-MacBook-Air.local>
2022-06-23 01:43:35 +0800

    added authorization to routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import User from "./pages/user/UserPage";
 import LoginPage from "./pages/login/LoginPage";
 import Dashboard from "./pages/Dashboard/DashboardPage";
 import { Route, Routes } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+import { UserRoleEnum } from "./models/UserRoleEnum";
 
 const App: React.FC = () => {
+  const {
+    getLoggedInUserRole,
+    isAuthorized
+  } = useAuth();
+
   return (
     <div className={"App"}>
       <Routes>
         <Route path="/" element={<LoginPage />}></Route>
-        <Route path='/index' element={<Index />} />
-        <Route path='/user' element={<User />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        {getLoggedInUserRole() && <Route path='/index' element={<Index />} />}
+        {isAuthorized([UserRoleEnum.SUPER_ADMIN, UserRoleEnum.ADMIN, UserRoleEnum.EDITOR]) && <Route path='/user' element={<User />} />}
+        {getLoggedInUserRole() &&
+          <Route path='/dashboard' element={<Dashboard />} />}
       </Routes>
     </div>
   );
