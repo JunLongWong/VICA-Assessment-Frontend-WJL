31fdff92c380a72ee1ef16e2312717ab04245470
Jun Long <junlong@Juns-MacBook-Air.local>
2022-07-01 13:10:13 +0800

    edited naming convention

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Index from "./pages/book/IndexPage";
+import Index from "./pages/book/indexPage";
 import User from "./pages/user/UserPage";
-import LoginPage from "./pages/login/LoginPage";
-import Dashboard from "./pages/Dashboard/DashboardPage";
+import LoginPage from "./pages/login/loginPage";
+import Dashboard from "./pages/Dashboard/dashboardPage";
 import { Route, Routes } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth";
 import { UserRoleEnum } from "./models/UserRoleEnum";
+// import BorrowHistory from "./pages/borrowHistory/borrowHistory";
 
 const App: React.FC = () => {
   const {
@@ -20,7 +21,11 @@ const App: React.FC = () => {
         {getLoggedInUserRole() && <Route path='/index' element={<Index />} />}
         {isAuthorized([UserRoleEnum.SUPER_ADMIN, UserRoleEnum.ADMIN, UserRoleEnum.EDITOR]) && <Route path='/user' element={<User />} />}
         {getLoggedInUserRole() &&
-          <Route path='/dashboard' element={<Dashboard />} />}
+          <>
+            <Route path='/dashboard' element={<Dashboard />} />
+            {/* <Route path='/borrowhistory' element={<BorrowHistory />} /> */}
+          </>
+        }
       </Routes>
     </div>
   );
